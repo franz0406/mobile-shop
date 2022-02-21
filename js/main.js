e23f21f5ba8cdcff597e1589b722e23af171f92b
hwang <franz0406@naver.com>
2022-02-21 23:11:58 +0900

    login page up

--- js/main.js
+++ js/main.js
@@ -1,9 +1,46 @@
 window.addEventListener('DOMContentLoaded', function(){
 
-    // 메인페이지 슬라이더
-    const swiper변수명 = new Swiper(".main_swiper", {
+    // onboarding page swiper
+    const s = new Swiper(".swiper", {
         autoplay: {
             delay: 4000,
+            disableOnInteraction:false
+        },
+        spaceBetween: 20,
+        loop:true,
+        speed:1000,
+        pagination: {
+            el: '.pagination',
+            type: 'bullets',
+            bulletClass:'bullet', 
+            bulletActiveClass:'bullet-active'
+        },
+    })
+
+
+    // onboarding page swiper
+    const mainSwiper = new Swiper(".main_swiper", {
+        autoplay: {
+            delay: 4000,
+            disableOnInteraction:false
+        },
+        spaceBetween: 20,
+        loop:true,
+        speed:1000,
+        pagination: {
+            el: '.pagination',
+            type: 'bullets',
+            bulletClass:'bullet', 
+            bulletActiveClass:'bullet-active'
+        },
+    })
+
+
+    // login page swiper
+    const loginSwiper = new Swiper(".login-swiper", {
+        autoplay: {
+            delay: 4000,
+            disableOnInteraction:false
         },
         spaceBetween: 20,
         loop:true,
