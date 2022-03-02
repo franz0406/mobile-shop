d5a19eea8ff1dc016b03528f6ec2463cb2f18bba
hwang <franz0406@naver.com>
2022-03-03 00:26:54 +0900

    my cart script finish

--- js/mycart.js
+++ js/mycart.js
@@ -1,22 +1,67 @@
 window.addEventListener("DOMContentLoaded", function(){
-    const cartList = document.querySelectorAll('.cart-list > li');
-    
-    //총 결제 금액
+
     const totalPrice = document.querySelector('.total-price > .price');
     const shippingCost = parseInt(document.querySelector('.shipping > .price').innerText.replace("$", "").trim());;
-    let setTotalPrice = 0;
-    let grandTotalPrice = totalPrice + shippingCost;
 
     // 페이지 열릴때 결제 금액 계산
     calcTotalPrice();
 
+    // 페이지 열릴때 아이템 개수 계산
+    itemListCounter();
+
     // 수량이 바꾸면 합계 다시 계산
-    document.querySelectorAll('.quantity > input[type="number"').addEventListener('change', calcTotalPrice);
-    
+    const quantityInput = document.querySelectorAll('.quantity > input');
+    quantityInput.forEach( input =>{
+        input.addEventListener('change', ()=>{
+            calcTotalPrice();
+        });
+    })
+
+    // delete 버튼 으로 리스트 삭제시 합계 다시 계산
+    const deleteButtons = document.querySelectorAll('.cart-list > li > .delete');
+    deleteButtons.forEach( button => {
+        button.addEventListener('click', (e)=>{
+            deleteItems(e.target);
+            calcTotalPrice();
+            itemListCounter();
+        });
+    })
+
+    // 결제 금액 합계 함수
     function calcTotalPrice(){
-        cartList.forEach((item, idx)=>{
-            const inputNum = item.querySelector('input');
-        })
+        const cartList = document.querySelectorAll('.cart-list > li');
+        let setTotalPrice = 0;
+
+        if(cartList.length === 0) {
+            totalPrice.innerText = '$ 0';
+            document.querySelector('.shipping > .price').innerText = '$ 0';
+        } else {
+            cartList.forEach((item, idx)=>{
+                const price = parseInt(item.querySelector('h5').innerText.replace("$", "").trim());
+                const quantity = parseInt(item.querySelector('input').value);
+                
+                setTotalPrice += price * quantity;
+                totalPrice.innerText = `$ ${(setTotalPrice + shippingCost).toLocaleString('en').replace(/,/g, ".")}.00`;
+            })
+        }
+
+    }
+
+    // 리스트 개수 함수
+    function itemListCounter(){
+        const cartList = document.querySelectorAll('.cart-list > li');
+        const itemCount = document.querySelector('.item-count > .count');
+        const listLength = cartList.length;
+
+        if(listLength == 0) return itemCount.innerText = 'There is no';
+        itemCount.innerText = listLength;
+
+    }
+
+    // 리스트 삭제 함수
+    function deleteItems(e) {
+        const userConfirm = confirm("delete this item?");
+        if(userConfirm == true) e.parentElement.remove();
     }
 
 })
